d358aff480c48771c3953c23c2ada6406941fd25
Richard Autry <autry.richard@gmail.com>
2023-07-06 06:31:01 -0400

    Initial Commit

---
+++ EnergyApp-Prototype/EnergyApp_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyApp_PrototypeApp.swift
+//  EnergyApp-Prototype
+//
+//  Created by Richard Autry on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EnergyApp_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
