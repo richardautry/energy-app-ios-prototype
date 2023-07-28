26824f55866a110d69051c514c0d7311b46566e6
Richard Autry <autry.richard@gmail.com>
2023-07-28 19:58:12 -0400

    Started view for automations

--- EnergyApp-Prototype/Views/FullDeviceAutomationsView.swift
+++ EnergyApp-Prototype/Views/FullDeviceAutomationsView.swift
@@ -5,4 +5,20 @@
 //  Created by Richard Autry on 7/27/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct FullDeviceAutomationsView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Full Device Automations")
+            Text("Turn Off Timer")
+            Text("Energy Savings")
+        }
+    }
+}
+
+struct FullDeviceAutomationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FullDeviceAutomationsView()
+    }
+}
