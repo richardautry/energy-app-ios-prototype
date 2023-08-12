9b733bdeb69e880c00c09a1c3e0049c154fdcd0c
Richard Autry <autry.richard@gmail.com>
2023-08-11 21:10:52 -0400

    Blocked on backgroundtasks

--- EnergyApp-Prototype/EnergyApp_PrototypeApp.swift
+++ EnergyApp-Prototype/EnergyApp_PrototypeApp.swift
@@ -14,7 +14,11 @@ struct EnergyApp_PrototypeApp: App {
         WindowGroup {
             ContentView()
         }
-        .backgroundTask(.appRefresh("TurnOffDevice")) {
+        .backgroundTask(
+            // TOOD: Not only is this not working, but background tasks in general don't work as expected.
+            // The OS can decide to not run the task depending on user interaction with the app
+            // This is not the way forward for now.
+            .appRefresh("TurnOffDevice")) {
             // scheduleAppRefresh()
             print("THIS IS THE BACKGROUND TASK FIRING")
             // TODO: Add turn_off function here
@@ -22,11 +26,11 @@ struct EnergyApp_PrototypeApp: App {
         }
     }
     
-//    init() {
-//        BGTaskScheduler.shared.register(forTaskWithIdentifier: "TurnOffDevice", using: nil, launchHandler: {task in
-//            print("THIS IS A BACKGROUND TASK SCHEDULER FIRIING")
-//        })
-//    }
+    init() {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "TurnOffDevice", using: nil, launchHandler: {task in
+            print("THIS IS A BACKGROUND TASK SCHEDULER FIRIING")
+        })
+    }
     
 //    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 //        let feedVC = (window?.rootViewController as? UINavigationController)?.viewControllers.first as? FeedTableViewController
@@ -50,8 +54,10 @@ struct EnergyApp_PrototypeApp: App {
 }
 
 func scheduleAppRefresh() {
-    let today = Calendar.current.startOfDay(for: .now)
-    let nextDate = Calendar.current.date(byAdding: .second, value: 1, to: today)
+    let today = Date()
+    print("\(today)")
+    let nextDate = Calendar.current.date(byAdding: .second, value: 30, to: today)
+    print("Next Date:\(nextDate!)")
 //    let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: today)!
 //    let noonComponent = DateComponents(hour: 12)
 //    let noon = Calendar.current.date(byAdding: noonComponent, to: tomorrow)
