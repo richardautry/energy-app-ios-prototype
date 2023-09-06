0a21f42903ce960bc7a1e5e82cbd47dc7a813353
Richard Autry <autry.richard@gmail.com>
2023-09-05 21:46:05 -0400

    Started new file for Rust calls

---
+++ EnergyApp-Prototype/Models/RustServer.swift
@@ -0,0 +1,10 @@
+//
+//  RustServer.swift
+//  EnergyApp-Prototype
+//
+//  Created by Richard Autry on 9/5/23.
+//
+
+import Foundation
+
+// TODO: Create calls to Rust Server here for automation starts using timer and grid sync
